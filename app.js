4fe4899c9aa6bb803d88bd1041f11d44eba26101
volkovakv <kv.volkova93@yandex.ru>
2022-04-22 09:39:41 +0300

    fix: исправить ошибки

--- app.js
+++ app.js
@@ -30,10 +30,7 @@ app.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string()
+      password: Joi.string().required()
         .regex(
           /^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\\/])*)?/,
         ),
